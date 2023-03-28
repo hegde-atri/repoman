0f5882c8a328c1f84a357c182183211ac92afd4b
Atri Hegde <iamatrihegde@outlook.com>
2023-03-28 17:32:48 +0100

    (feat): finished command::exec

--- src/utils/command.rs
+++ src/utils/command.rs
@@ -1,14 +1,60 @@
 use std::{
-    io,
-    io::Error,
-    io::Write,
+    io::{Error, ErrorKind},
+    path::Path,
     process::{Command, Output},
 };
 
-pub fn exec(cmd: &str) -> Result<Output, Error> {
+use which::which;
+
+/// Returns output to give command
+///
+/// It takes in the command/binary to execute, optional flags and the path
+/// to execute it in.
+///
+/// It will only execute if the provided `cmd` is found on host and `cwd` exists
+/// as a directory.
+///
+/// # Arguments
+///
+/// * `cmd` - The binary to execute.
+/// * `args` - Optional to include arguments.
+/// * `cwd` - The directory to execute in.
+///
+/// # Examples
+/// ```
+/// exec("git", Some("status"), Path::new("/home/user/repo"))
+/// ```
+pub fn exec(cmd: &str, args: Option<&str>, cwd: &Path) -> Result<Output, Error> {
+    // Check if binary for command exists
+    match which(cmd) {
+        Ok(_) => (),
+        Err(_) => {
+            return Err(Error::new(
+                ErrorKind::NotFound,
+                format!("Could not find specified command: {}", cmd),
+            ))
+        }
+    }
+    // Check if path exists
+    if !cwd.exists() {
+        return Err(Error::new(
+            ErrorKind::Other,
+            "Specified directory is invalid!",
+        ));
+    }
+    // Now execute the command
     if cfg!(target_os = "windows") {
-        return Command::new("cmd").args(["/C", cmd]).output();
+        return Command::new("cmd")
+            .current_dir(&cwd.as_os_str())
+            .args(["/C", cmd])
+            .arg(args.unwrap_or(""))
+            .output();
     } else {
-        return Command::new("sh").arg("-c").arg(cmd).output();
+        return Command::new("sh")
+            .current_dir(&cwd.as_os_str())
+            .arg("-c")
+            .arg(cmd)
+            .arg(args.unwrap_or(""))
+            .output();
     };
 }
